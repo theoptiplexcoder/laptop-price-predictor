30f3f0c033e3842a4f56f002b7c43bf9a96973e2
Your Name <you@example.com>
2025-11-01 14:01:10 +0530

    laptop price predictor

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
 from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
@@ -7,7 +8,7 @@ import numpy as np
 
 app = Flask(__name__)
 cors=CORS(app)
-model = pickle.load(open('LinearRegressionModelLaptop.pkl','rb'))
+model = pickle.load(open('LinearRegressionModelLaptop.pkl', 'rb'))
 laptop = pd.read_csv('cleaned_laptops.csv')
 
 @app.route("/")
@@ -21,13 +22,13 @@ def index():
     hdd = sorted(laptop['hdd'].unique())
     ssd = sorted(laptop['ssd'].unique())
     os = sorted(laptop['os'].unique())
-    os_bit=64
     graphics = sorted(laptop['graphic_card_gb'].unique())
     weight=sorted(laptop['weight'].unique())
     warranty= sorted(laptop['warranty'].unique())
     touchscreen= sorted(laptop['Touchscreen'].unique())
     msoffice= sorted(laptop['msoffice'].unique())
-    ratings= sorted(laptop['Number of Ratings'].unique())
+    ratings= sorted(laptop['rating'].unique())
+    number_of_ratings= sorted(laptop['Number of Ratings'].unique())
     number_of_reviews= sorted(laptop['Number of Reviews'].unique())
 
     context = {
@@ -40,18 +41,67 @@ def index():
     'hdds': hdd,
     'ssds': ssd,
     'oss': os,
-    'os_bits': os_bit,
     'graphics': graphics,
     'weights': weight,
     'warrantys': warranty,
     'touchscreens': touchscreen,
     'msoffices': msoffice,
-    'ratings': ratings,
+    'rating': ratings,
+    'number_of_ratings': number_of_ratings,
     'number_of_reviews': number_of_reviews
     }
 
     return render_template('index.html', **context)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get form or query parameters (works for both GET and POST)
+    brand = request.values.get('brand')
+    processor_brand = request.values.get('processor_brand')
+    processor_name = request.values.get('processor_name')
+    processor_gen = request.values.get('processor_gen')
+    ram_gb = request.values.get('ram_gb')
+    ram_type = request.values.get('ram_type')
+    hdd = request.values.get('hdd')
+    ssd = request.values.get('ssd')
+    graphics = request.values.get('graphics')
+    weight = request.values.get('weight')
+    warranty = request.values.get('warranty')
+    touchscreen = request.values.get('touchscreen')
+    os=request.values.get('os')
+    msoffice = request.values.get('msoffice')
+    ratings = request.values.get('ratings')
+    number_of_ratings = request.values.get('number_of_ratings')
+    number_of_reviews = request.values.get('number_of_reviews')
 
+    # Create a DataFrame for model prediction
+
+    input_data = pd.DataFrame([{
+        'brand': brand,
+        'processor_brand': processor_brand,
+        'processor_name': processor_name,
+        'processor_gnrtn': processor_gen,
+        'ram_gb': ram_gb,
+        'ram_type': ram_type,
+        'ssd': ssd,
+        'hdd': hdd,
+        'os': os,
+        'graphic_card_gb': graphics,
+        'weight': weight,
+        'warranty': warranty,
+        'Touchscreen': touchscreen,
+        'msoffice': msoffice,
+        'rating': ratings,
+        'Number of Ratings': number_of_ratings,
+        'Number of Reviews': number_of_reviews,
+    }])
+    # Get prediction from model
+    prediction = model.predict(input_data)[0]
+    print(prediction)
+    return f"<div class='result-box'>💻 Estimated Price: <b>{prediction}</b> ₹</div>"
+
+    # Return as JSON
 if __name__ == '__main__':
     app.run(debug=True)
+
+
